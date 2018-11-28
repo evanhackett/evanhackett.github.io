0a0e8f235b2f944fcc6a2b0f92ba017ee8f8b37e
evanhackett <evanwhackett@gmail.com>
2018-11-28 13:43:07 -0800

    added purgecss and some npm scripts to build the css file.

---
+++ purgecss.config.js
@@ -0,0 +1,19 @@
+// define extractor class
+// tailwin uses ":" in media queries, so we need a custom extractor.
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+
+module.exports = {
+  content: ['index.html'],
+  css: ['output.css'],
+  extractors: [
+    {
+      extractor: TailwindExtractor,
+      extensions: ['html']
+    }
+  ]
+}
