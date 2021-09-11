935e1f1425d9758849561644c531bd2e9353baa4
evanhackett <evanwhackett@gmail.com>
2021-09-10 23:31:15 -0700

    centered social icons and change header font. also change animation to on-hover instead of click.

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 const socialIcons = [
   {
     dom: document.getElementById("octocat"),
-    link: "https://github.com/evanhackett",
     description: "Github - see my code and open source activity"
   },
   {
     dom: document.getElementById("blog"),
-    link: "https://evanhackett.com/blog",
     description: "Blog - I write about all things software development"
   },
   {
     dom: document.getElementById("twitter"),
-    link: "https://twitter.com/hackett_evan",
     description: "twitter - I might tweet more if I had more followers!"
   },
   {
     dom: document.getElementById("linkedin"),
-    link: "https://linkedin.com/in/hackettevan",
     description: "LinkedIn - I heard you're supposed to have one of these..."
   },
 ]
 
-socialIcons.forEach(item => {
-  // set up click handlers for links
-  item.dom.onclick = e => {
-    setInterval(() => {
-      // increase speed of stars. Doing so on an interval causes an acceleration effect.
-      speed += 2
-    }, 50)
-    setTimeout(() => {
-      location.assign(item.link);
-    }, 1500)
+function accelerate() {
+  for (let i = 0; i < 80; i++) {
+    setTimeout(() => speed += 0.5, 50 * i)
   }
+}
 
+function decelerate() {
+  for (let i = 0; i < 80; i++) {
+    setTimeout(() => speed -= 0.5, 50 * i)
+  }
+}
+
+const workExperience = document.getElementById('work-exp')
+
+socialIcons.forEach(item => {
   // set up on hover handlers for link images.
-  // On hover will cause text to appear that explains what the link is.
   const linkDescription = document.getElementById("link-description")
-
+  
+  let intervalId;
+  
   item.dom.onmouseover = () => {
+    // On hover will cause text to appear that explains what the link is.
     linkDescription.innerHTML = item.description
+    
+    // It will also speed up the star animation.
+    accelerate()
+
+    // lastly it will fade out the work experience section text
+    workExperience.classList.add('fade-out')
+    workExperience.classList.remove('fade-in')
   }
   item.dom.onmouseout = () => {
-    linkDescription.innerHTML = ''  
+    linkDescription.innerHTML = ''
+    decelerate()
+    workExperience.classList.remove('fade-out')
+    workExperience.classList.add('fade-in')
   }
 })
-
